516e94971b040d2169b0f119cc2f9f61c663ca5b
Wuttinan Chaoyos <wuttinan.wch@gmail.com>
2018-04-23 17:34:43 +0700

    add feature mute command

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const {
 
 var simsimi = new Simsimi(simsimiOption);
 
+var arrMuteBot = [];
 client.on('ready', function (message) {
     let today = new Date();
     let curHr = today.getHours();
@@ -22,19 +23,78 @@ client.on('ready', function (message) {
     }
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`Nothing`);
+    client.user.setActivity(`ดูดกัญชา`);
     var channel = client.channels.get('300568765001891843');
     channel.sendMessage('@everyone บอทมาแล้วจ้าาาาาาา ' + greetMsg + ':pray::skin-tone-3: :pray::skin-tone-3: :pray::skin-tone-3:' )
+    //channel.sendMessage('อย่ายุ่งกะกูกูงอล ชิชิชิ' )
+    //channel.sendMessage('บอทไปก่อนน้าาาา บุ้ยบุย...' )
 });
+
+function search(key, array, remove) {
+    if (remove) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === key) {
+                console.log('remove : ', arrMuteBot[i]);
+                arrMuteBot.splice(i, 1);
+                console.log('array  : ', arrMuteBot);
+                //return array[i];
+                return true;
+            }
+        }
+        return false;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === key) {
+                //return array[i];
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
 
 client.on('message', function (message) {
     if (message.author.bot) return;
-    console.log('message ', message.content)
-    simsimi.listen(message.content, function (err, msg) {
-        if (err) return console.error(err);
-        console.log('simsimi say : ', msg)
-        message.reply(msg);
-    })
+    console.log('message ', message.content);
+
+    
+    
+    if (message.content == '!unmute') {
+        if (search(message.author.id, arrMuteBot, true)) {
+            console.log('removed : ', arrMuteBot.length);
+            message.reply('ควยยยยบังอาจ mute กู คิดถึงกูละซิ้');
+        } else {
+            return;
+        }
+
+    }
+
+    //check user mute bot? if true not response that user.
+    if (search(message.author.id, arrMuteBot, false)) return;
+
+    if (message.content == '!mute') {
+        if (search(message.author.id, arrMuteBot, false)) return;
+        arrMuteBot.push(message.author.id);
+        console.log('add user id : ', message.author.id);
+        console.log('arr ', arrMuteBot);
+        message.reply('ชิชิชิ บังบาจ mute เค้าไปกะได้ *หากต้องการ unmute พิมพ์ !unmute');
+    } else {
+        simsimi.listen(message.content, function (err, msg) {
+            if (err) {
+                console.log(err);
+                if (err.result === 404) {
+                    let arrNotFound = ["พิมพ์ควยไรกูไม่เข้าใจ","...","หนูไม่เข้าใจที่คุณพูดค่ะ"];
+                    //message.reply(arrNotFound[Math.floor(Math.random() * arrNotFound.length)]);
+                }
+            }else {
+                console.log('simsimi say : ', msg)
+                message.reply(msg);
+            }
+
+        });
+    }
+
 })
 
 client.login(token);
