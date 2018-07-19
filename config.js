350ca00686eb085cd5446af3860407b1c1c8da52
Wuttinunt Chaoyos <b5601233@office365.sut.ac.th>
2018-07-19 21:46:48 +0700

    update more security app

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: process.env.BOT_TOKEN || "",
+    channelChatbotId: process.env.DISCORD_CHANNEL_ID || "",
+    simsimiOption: {
+        lc: process.env.SIM_LC || "",
+        ft: process.env.SIM_FT || "",
+        key: process.env.SIM_KEY || ""
+    }
+}
