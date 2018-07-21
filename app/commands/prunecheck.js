0831ae440a215926c2c485738204e2964c9334b5
Wuttinan Chaoyos <wuttinan.wch@gmail.com>
2018-07-22 00:51:39 +0700

    add feature prune member and add bot chat multi channel

---
+++ app/commands/prunecheck.js
@@ -0,0 +1,26 @@
+const R = require('ramda')
+const constants = require('../constants')
+exports.run = async (client, message, days) => {
+    const hasPermission = message.member.hasPermission([constants.ADMINISTRATOR], false, true)
+    if (!hasPermission)
+        return message.reply("You haven't permission to use this command.")
+
+    const daysToInt = parseInt(days[0])
+    const isNumber = R.both(R.is(Number), R.complement(R.equals(NaN)))(daysToInt)
+    if (!isNumber)
+        return message.reply("จำนวนวันต้องเป็นตัวเลขและอยู่ระหว่าง 1 ถึง 30")
+    if (daysToInt < 1 || daysToInt > 30)
+        return message.reply("โปรดระบุจำนวนวันระหว่าง 1 ถึง 30")
+    try {
+        const daysInt = parseInt(daysToInt)
+        daysInt && message.guild.pruneMembers(daysInt, true)
+            .then(pruned => {
+                console.log(`member จำนวน ${pruned} คน ไม่ออนไลน์ในระยะเวลา ${days[0]} วัน`)
+                return message.reply(`member จำนวน ${pruned} คน ไม่ออนไลน์ในระยะเวลา ${days[0]} วัน`)
+            })
+            .catch(console.error);
+    } catch (err) {
+        console.log(err)
+    }
+
+}
--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 const constants = require('./constants')
 module.exports = {
     token: process.env.BOT_TOKEN || "NDY5NTA4MzM2NDIxOTYxNzM4.DjRv8Q.IZhgay6JPHEY9FvtBFStluAKxJE",
-    channelChatbotId: process.env.DISCORD_CHANNEL_ID || "362987223454449665",
+    mainChannelIDChatbot: process.env.DISCORD_CHANNEL_ID || "362987223454449665",
     simsimiOption: {
         lc: process.env.SIM_LC || "th",
         ft: process.env.SIM_FT || "0.0",
@@ -9,5 +9,6 @@ module.exports = {
     },
     prefix: process.env.PREFIX || "!",
     ownerBotRole: [constants.MANAGE_CHANNELS],
-    activity: process.env.BOT_ACTIVITY || ""
+    activity: process.env.BOT_ACTIVITY || "",
+    multiChannel: process.env.MULTI_CHANNEL || true
 }
