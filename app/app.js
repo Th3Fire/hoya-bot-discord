409d42232251cf5d6a9e679cf0c7b870283e49f7
Wuttinan Chaoyos <wuttinan.wch@gmail.com>
2018-07-27 00:26:13 +0700

    remove console.log

--- app/app.js
+++ app/app.js
@@ -126,10 +126,6 @@ client.on('message', async message => {
         return;
     }
 
-    console.log('message.channel.name : ', message.channel.name)
-    console.log('botChannelName : ', botChannelName)
-    console.log('multiChannel : ', multiChannel)
-
     if (message.channel.name !== botChannelName.toString() && !multiChannel) return;
 
     if (message.content.startsWith(prefix)) {
