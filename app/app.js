89939ee0bea9e929f6480182980c7f66f9a041b0
Wuttinan Chaoyos <16854458+wuttinanDEV@users.noreply.github.com>
2018-07-27 01:56:50 +0700

    fixed defect check condition chat multi channel

--- app/app.js
+++ app/app.js
@@ -125,12 +125,7 @@ client.on('message', async message => {
         }
         return;
     }
-    console.log('typeof multi : ', typeof multiChannel)
-    console.log('typeof botChannelName : ', typeof botChannelName)
-    console.log('message.channel.name : ', message.channel.name)
-    console.log('botChannelName : ', botChannelName)
-    console.log('multiChannel : ', multiChannel)
-    if ((message.channel.name !== botChannelName.toString()) && !multiChannel) return;
+    if ((message.channel.name !== botChannelName) && (multiChannel === 'N')) return;
 
     if (message.content.startsWith(prefix)) {
         const args = message.content.slice(prefix.length).trim().split(/ +/g)
