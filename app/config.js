1207316c04e2d3a763fc6dc3112651e3645a57ec
Wuttinan Chaoyos <wuttinan.c@appman.co.th>
2019-09-20 17:30:19 +0700

    add filter env

--- app/config.js
+++ app/config.js
@@ -2,12 +2,12 @@ const constants = require('./constants')
 const { key } = require('./key/sim-key.json')
 
 module.exports = {
-    token: process.env.BOT_TOKEN || "",
+    token: process.env.BOT_TOKEN || "NDcwMjk3NTQ0NDMwMTkwNjEy.XYSpxg.ajuOzp22KhnfG6WLgo8nrZsXHlU",
     mainChannelIDChatbot: process.env.DISCORD_CHANNEL_ID || "",
     simsimiOption: {
         lang: process.env.SIM_LC || "th",
         key: process.env.SIM_KEY || key || "",
-        filter: '1.0'
+        filter: process.env.FILTER || '1.0'
     },
     prefix: process.env.PREFIX || "!",
     ownerBotID: process.env.OWNER_BOT_ID || "",
