53116697ae5be3fe5bea00e11acf9a5fed268ef5
Wuttinan Chaoyos <wuttinan.wch@gmail.com>
2020-08-23 15:49:38 +0700

    update

--- src/index.js
+++ src/index.js
@@ -268,15 +268,18 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 })
 
 client.on('channelDelete', channel => {
-    db.VoiceChannel.findOne({
-        where: {
-            guildID: channel.guild.id,
-            channelID: channel.id
-        }
-    })
-    .then(vc => {
-        if (vc) vc.destroy()
-    })
+    if (channel.type === 'voice') {
+        db.VoiceChannel.findOne({
+            where: {
+                guildID: channel.guild.id,
+                channelID: channel.id
+            }
+        })
+        .then(vc => {
+            if (vc) vc.destroy()
+        })
+    }
+    
 })
 
 client.login(token)
